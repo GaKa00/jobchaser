dc29b342c62580e0a64eab2232f3a3610a97eff2
GaKa00 <145324695+GaKa00@users.noreply.github.com>
2024-03-07 13:07:27 +0100

    initialised project

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,26 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <section>
+      {/* Searchbar */}
+    </section>
+    <main>
+      {/* jobCard */}
+    </main>
+  )
+}
+
+
+function Searchbar() {
+  return(
+    <span className="searchbar">Search</span>
+  )
+}
+
+
+
+export default App
