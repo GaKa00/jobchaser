cc2632b3c6ec057e31ea2678d1c816b4f6c075cc
GaKa00 <145324695+GaKa00@users.noreply.github.com>
2024-03-08 13:25:09 +0100

    edited css

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,26 +1,90 @@
-
-import './App.css'
+import jobdata from "./data";
+import "./App.css";
 
 function App() {
-  
-
   return (
-    <section>
-      {/* Searchbar */}
-    </section>
-    <main>
-      {/* jobCard */}
-    </main>
-  )
-}
+    <>
+<div className="banner"></div>
+      <section>
+        <Searchbar />
+      </section>
 
+      <main>
+        <Jobcard jobdata={jobdata} />
+      </main>
+    </>
+  );
+}
 
 function Searchbar() {
-  return(
-    <span className="searchbar">Search</span>
-  )
+  return <span className="searchbar">Search</span>;
 }
 
+type Job = {
+  id: number;
+  company: string;
+  logo: string;
+  position: string;
+  role: string;
+  level: string;
+  postedAt: string;
+  contract: string;
+  location: string;
+  languages: string[];
+  tools: string[];
+};
+
+function Jobcard({ jobdata }: { jobdata: Job[] }) {
+  const jobLang = jobdata.map(jobObj => {
+     return jobObj.languages.map(lang => <div className="tagsL">#{lang}</div> )
+    
+  } )
+    const jobTool = jobdata.map((jobObj) => {
+      return jobObj.tools.map((tool) => (
+        <div className="tagsT">#{tool}</div>
+      ));
+    });
+  
+
+  
+  
 
 
-export default App
+
+  return jobdata.map((job, index) => (
+    <div key={job.id}>
+      <div>{job.id}</div>
+      <div className="card-container">
+        <img src={job.logo} alt={job.position} />
+        <div className="position-container">
+          <h3>
+            {job.position} at {job.company}
+          </h3>
+          <div className="info-container">
+            <div className="info">
+              {" "}
+              Role: <br /> {job.role}
+            </div>
+            <div className="info">
+              {" "}
+              Level: <br /> {job.level}
+            </div>
+            <div className="info">
+              {" "}
+              Contract: <br /> {job.contract}
+            </div>
+            <div className="info">
+              {" "}
+              Location: <br /> {job.location}
+            </div>
+          </div>
+        </div>
+        <div className="tag-container">
+          {jobLang[index]} {jobTool[index]}
+        </div>
+      </div>
+    </div>
+  ));
+}
+
+export default App;
