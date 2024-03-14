bf9ff65b9e5c8a88b2144d73be40cdca494f8908
GaKa00 <145324695+GaKa00@users.noreply.github.com>
2024-03-14 10:33:23 +0100

    finished week 1 and almost week 2

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,24 +1,6 @@
 import jobdata from "./data";
 import "./App.css";
-
-function App() {
-  return (
-    <>
-<div className="banner"></div>
-      <section>
-        <Searchbar />
-      </section>
-
-      <main>
-        <Jobcard jobdata={jobdata} />
-      </main>
-    </>
-  );
-}
-
-function Searchbar() {
-  return <div className="searchbar">Search</div>;
-}
+import { useState } from "react";
 
 type Job = {
   id: number;
@@ -33,29 +15,100 @@ type Job = {
   languages: string[];
   tools: string[];
 };
+{
+  /* App tar en usestate på filtreradejob, defaultas som jobdata i formen job[]  för att visa alla element till start
+app skapar en funktion handleSearch som tar enot filtrerad data och settar filtreradejobb till filtrerad data
+
+Searchbar Tar emot  jobdata, och  handlesearch funktion som props.
+Jobdata för att ha tillgång till den data som ska filtreras, och handleSearch för att passa tillbaka till APP
+
+
+Searchbar typar propsen, och definerar en state för att ta emot input som skrivs
+Searchbar skapar en funktion handleChange för att hantera ändring till input.
+
+handleChange en variabel searchterm, vilket är lika med settad search input
+i handleChange skapar en filter metod på jobdata, som jämför jobdata med searchinput
+
+efter filtrerad data, passeras den tillbaka till App via handleSearch
+den uppdaterade handleSearch settar filtreradeJobs
+den uppdaterade filtreradeJobs passeras till Jobcard, och visar nu endast de filtrerade job som Jobcards.
+
+*/
+}
+
+function App() {
+  const [filteredJobs, setFilteredJobs] = useState<Job[]>(jobdata);
+
+  const handleSearch = (filteredData: Job[]) => {
+    setFilteredJobs(filteredData);
+  };
+
+  return (
+    <>
+      <div className="banner"></div>
+      <section>
+        <Searchbar jobdata={jobdata} handleSearch={handleSearch} />
+      </section>
+
+      <main>
+        <Jobcard jobdata={filteredJobs} />
+      </main>
+    </>
+  );
+}
+
+function Searchbar({
+  jobdata,
+  handleSearch,
+}: {
+  jobdata: Job[];
+  handleSearch: (filteredData: Job[]) => void;
+}) {
+  const [searchInput, setSearchInput] = useState("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const searchTerm = e.target.value.trim().toLowerCase();
+    setSearchInput(searchTerm);
+
+    const filterJobs = jobdata.filter((job: Job) => {
+      return (
+        job.company.toLowerCase().includes(searchTerm) ||
+        job.level.toLowerCase().includes(searchTerm) ||
+        job.position.toLowerCase().includes(searchTerm) ||
+        job.contract.toLowerCase().includes(searchTerm) ||
+        job.role.toLowerCase().includes(searchTerm)
+      );
+    });
+
+    handleSearch(filterJobs);
+  };
+
+  return (
+    <section>
+      <label>Search</label>
+      <input
+        type="text"
+        placeholder="Search Jobs..."
+        onChange={handleChange}
+        value={searchInput}
+      />
+    </section>
+  );
+}
 
 function Jobcard({ jobdata }: { jobdata: Job[] }) {
-  const jobLang = jobdata.map(jobObj => {
-     return jobObj.languages.map(lang => <div className="tagsL">#{lang}</div> )
-    
-  } )
-    const jobTool = jobdata.map((jobObj) => {
-      return jobObj.tools.map((tool) => (
-        <div className="tagsT">#{tool}</div>
-      ));
-    });
-  
-
-  
-  
-
-
+  const jobLang = jobdata.map((jobObj) => {
+    return jobObj.languages.map((lang) => <div className="tagsL">#{lang}</div>);
+  });
+  const jobTool = jobdata.map((jobObj) => {
+    return jobObj.tools.map((tool) => <div className="tagsT">#{tool}</div>);
+  });
 
   return jobdata.map((job, index) => (
     <div key={job.id}>
-      <div>{job.id}</div>
       <div className="card-container">
-        <img src={job.logo} alt={job.position} />
+        <img className="jobImage" src={job.logo} alt={job.position} />
         <div className="position-container">
           <h3>
             {job.position} at {job.company}
---
+++ vite-project/teori.md
@@ -0,0 +1,13 @@
+Allmänt om ramverket React: Hur/Varför uppkom det? Vad är dess kännetecken?
+Vad är JSX?
+Vad är en komponent i React?
+Vad är props i React?
+Vad menas med one-way-dataflow?
+Hur kan man använda sig av conditionell rendering i React?
+Vad är state i React?
+Vad är det för skillnad mellan state och props?
+Vad menas med en kontrollerad komponent i React?
+Vad är en callback handler?
+Vad menas med "lifting state up"?
+Vad är syftet med useEffect-hook i React?
+Vad är syftet kring den s.k dependency-arrayen i useEffect?
